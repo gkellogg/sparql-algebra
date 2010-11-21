c375e6df6f6ef0aa9a6bcd4de0618bdea349fcd0
Arto Bendiken <arto@bendiken.net>
2010-11-21 12:34:06 +0100

    Implemented the Expression.for and .new methods.

--- lib/sparql/algebra/expression.rb
+++ lib/sparql/algebra/expression.rb
@@ -4,6 +4,48 @@ module SPARQL; module Algebra
   #
   # @abstract
   module Expression
-    # TODO
+    ##
+    # @example
+    #   Expression.for(:isLiteral, RDF::Literal(3.1415))
+    #
+    # @param  [Array] sse
+    #   a SPARQL S-Expression (SSE) form
+    # @return [Operator]
+    def self.for(*sse)
+      self.new(sse)
+    end
+
+    ##
+    # @example
+    #   Expression.new([:isLiteral, RDF::Literal(3.1415)], :version => 1.0)
+    #
+    # @param  [Array] sse
+    #   a SPARQL S-Expression (SSE) form
+    # @param  [Hash{Symbol => Object}] options
+    # @return [Operator]
+    def self.new(sse, options = {})
+      operator = Operator.for(sse.first)
+      operands = sse[1..-1].map do |operand|
+        case operand
+          when Operator
+            operand
+          when Array
+            self.new(operand, options)
+          when RDF::Term
+            operand
+          when TrueClass, FalseClass, Numeric, String, DateTime, Date, Time
+            RDF::Literal(operand)
+          when Symbol then case
+              when operand.to_s[0] == ?? # for convenience
+                RDF::Query::Variable.new(operand.to_s[1..-1])
+              else
+                RDF::Literal(operand)
+            end
+          else
+            raise ArgumentError, "invalid SPARQL::Algebra::Expression operand: #{operand.inspect}"
+        end
+      end
+      operator.new(*operands)
+    end
   end # Expression
 end; end # SPARQL::Algebra
