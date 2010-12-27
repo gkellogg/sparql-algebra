7cef702079f17dc5a8e78e6daf7593321c03e236
Arto Bendiken <arto@bendiken.net>
2010-12-27 18:41:27 +0100

    Updated the spec helper for RSpec 2.x compatibility.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require 'sparql/algebra'
 require 'rdf/spec'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.include(RDF::Spec::Matchers)
+  config.exclusion_filter = {:ruby => lambda { |version|
+    RUBY_VERSION.to_s !~ /^#{version}/
+  }}
 end
 
 include SPARQL::Algebra
