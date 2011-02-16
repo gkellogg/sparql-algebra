d6e3b69a256c5b6be53561cb5b838f205c7edfc1
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-02-15 18:13:36 -0800

    Implement distinct.

---
+++ lib/sparql/algebra/operator/distinct.rb
@@ -0,0 +1,41 @@
+module SPARQL; module Algebra
+  class Operator
+    ##
+    # The SPARQL GraphPattern `distinct` operator.
+    #
+    # @see http://www.w3.org/TR/rdf-sparql-query/#sparqlAlgebra
+    class Distinct < Operator::Unary
+      include Query
+      
+      NAME = [:distinct]
+
+      ##
+      # Executes this query on the given `queryable` graph or repository.
+      # Really a pass-through, as this is a syntactic object used for providing
+      # context for URIs.
+      #
+      # @param  [RDF::Queryable] queryable
+      #   the graph or repository to query
+      # @param  [Hash{Symbol => Object}] options
+      #   any additional keyword options
+      # @return [RDF::Query::Solutions]
+      #   the resulting solution sequence
+      # @see    http://www.w3.org/TR/rdf-sparql-query/#sparqlAlgebra
+      def execute(queryable, options = {})
+        @solutions = operands.last.
+          execute(queriable, options = {}).
+          distinct
+      end
+      
+      ##
+      # Returns an optimized version of this query.
+      #
+      # Return optimized query
+      #
+      # @return [Union, RDF::Query] `self`
+      def optimize
+        operands = operands.map(&:optimize)
+      end
+    end # Distinct
+  end # Operator
+end; end # SPARQL::Algebra
