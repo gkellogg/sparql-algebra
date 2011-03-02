030e7e9b257a91b113536d4c0c08e8ce16bae9bb
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-02 10:06:18 -0800

    Operator::Equal is getting overly complex, but it satisfies expr-equal specs.

--- lib/sparql/algebra/operator/equal.rb
+++ lib/sparql/algebra/operator/equal.rb
@@ -44,16 +44,46 @@ module SPARQL; module Algebra
               # generate an error, but they don't compare otherwise
               RDF::Literal::FALSE
 
-            #when term1.datatype == term2.datatype && term1.language == term2.language
-            #  RDF::Literal(!!(term1 == term2) && term1.valid? && term2.valid?)
-
+            # If literals are syntactically equivalent
             # @see http://www.w3.org/TR/rdf-sparql-query/#func-RDFterm-equal
             when term1.eql?(term2)
               RDF::Literal::TRUE
 
-             # produces a type error if the arguments are both literal but are not the same RDF term
+            # Numerics and DateTimes can't be compared with other terms
+            when term1.is_a?(RDF::Literal::Boolean) ||
+                 term1.is_a?(RDF::Literal::Numeric) ||
+                 term1.is_a?(RDF::Literal::Date) ||
+                 term1.is_a?(RDF::Literal::Time) ||
+                 term1.is_a?(RDF::Literal::DateTime) ||
+                 term2.is_a?(RDF::Literal::Boolean) ||
+                 term2.is_a?(RDF::Literal::Numeric) ||
+                 term2.is_a?(RDF::Literal::Date) ||
+                 term2.is_a?(RDF::Literal::Time) ||
+                 term2.is_a?(RDF::Literal::DateTime)
+              raise TypeError, "can't compare #{term1.inspect} and #{term2.inspect}"
+
+            # If one of the terms is untyped, check for equivalence with an unknown typed term
+            # Fixme: not sure where this is defined, but implied by data-r2/expr-equal/eq-2-2
+            when term2.simple? && term1.has_datatype? && term1.datatype != RDF::XSD.string ||
+                 term1.simple? && term2.has_datatype? && term2.datatype != RDF::XSD.string
+              RDF::Literal::FALSE
+
+            # produces a type error if the arguments are both literal but are not the same RDF term
+            # @see http://www.w3.org/TR/rdf-sparql-query/#func-RDFterm-equal
             else raise TypeError, "unable to determine whether #{term1.inspect} and #{term2.inspect} are equivalent"
           end
+
+          # Numerics and DateTimes can't be compared with other terms
+          when term1.is_a?(RDF::Literal::Boolean) ||
+               term1.is_a?(RDF::Literal::Numeric) ||
+               term1.is_a?(RDF::Literal::Date) ||
+               term1.is_a?(RDF::Literal::Time) ||
+               term1.is_a?(RDF::Literal::DateTime) ||term1.is_a?(RDF::Literal::Boolean) ||
+               term2.is_a?(RDF::Literal::Numeric) ||
+               term2.is_a?(RDF::Literal::Date) ||
+               term2.is_a?(RDF::Literal::Time) ||
+               term2.is_a?(RDF::Literal::DateTime)
+            raise TypeError, "can't compare #{term1.inspect} and #{term2.inspect}"
 
           # @see http://www.w3.org/TR/rdf-concepts/#section-Graph-URIref
           # @see http://www.w3.org/TR/rdf-concepts/#section-blank-nodes
