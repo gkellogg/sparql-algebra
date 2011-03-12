6bc5424cb83fead40fa204b2e129a8cac15bc534
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-11 18:51:29 -0800

    Moved some inspectors to SPARQL::Spec.

--- spec/support/extensions/inspects.rb
+++ spec/support/extensions/inspects.rb
@@ -1,26 +1,5 @@
 # override several inspect functions to improve output for what we're doing
 
-class Array
-  alias_method :old_inspect, :inspect
-
-  def inspect
-    if all? { |item| item.is_a?(Hash) }
-      string = "[\n"
-      each do |item|
-        string += "  {\n"
-          item.keys.map(&:to_s).sort.each do |key|
-            string += "      #{key}: #{item[key.to_sym].inspect}\n"
-          end
-        string += "  },\n"
-      end
-      string += "]"
-      string
-    else
-      old_inspect
-    end
-  end
-end
-
 class RDF::Literal
   require 'rdf/ntriples'
   def inspect
