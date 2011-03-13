3c6f56734b28c8a7995fd236a975f86a0c0dc39c
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-13 13:30:36 -0700

    #to_sxp specs for core types.

--- spec/extensions_spec.rb
+++ spec/extensions_spec.rb
@@ -1,4 +1,30 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'sparql/algebra/sxp_extensions'
+
+describe "Core objects #to_sxp" do
+  [
+    [nil, RDF.nil],
+    [false, 'false'],
+    [true, 'true'],
+    ['', '""'],
+    ['string', '"string"'],
+    [:symbol, 'symbol'],
+    [1, '1'],
+    [1.0, '1.0'],
+    [BigDecimal("10"), '10.0'],
+    [1.0e1, '10.0'],
+    [Float::INFINITY, "+inf."],
+    [-Float::INFINITY, "-inf."],
+    [Float::NAN, "nan."],
+    [['a', 2], '("a" 2)'],
+    [Time.parse("2011-03-13T11:22:33Z"), '#@2011-03-13T11:22:33Z'],
+    [/foo/, '#/foo/'],
+  ].each do |(value, result)|
+    it "returns #{result.inspect} for #{value.inspect}" do
+      value.to_sxp.should == result
+    end
+  end
+end
 
 describe RDF::Queryable do
   context "#concise_bounded_description" do
